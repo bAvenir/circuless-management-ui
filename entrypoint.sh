2a9253e774748be5e439538f4ec370f03455e259
Matej Kokol <matej.kokol@centrum.sk>
2025-03-13 16:40:55 +0100

    added seeding to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ npx prisma migrate deploy
 
 # Seed the database
 echo "Seeding the database..."
-npx prisma db seed
+npx tsx prisma/seed.ts
 
 # Start the Node.js server
 echo "Starting Node.js server..."
