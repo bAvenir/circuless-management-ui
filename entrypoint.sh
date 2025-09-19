3b094f886a0053556aa97fbab8a6bd4118a459d8
Matej Kokol <matej.kokol@centrum.sk>
2025-09-19 12:33:27 +0200

    removed db seed from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,6 @@ npx prisma generate
 echo "Running Prisma migrations..."
 npx prisma migrate deploy
 
-# Seed the database
-echo "Seeding the database..."
-npx tsx prisma/seed.ts
-
 # Start the Node.js server
 echo "Starting Node.js server..."
 exec node .output/server/index.mjs
