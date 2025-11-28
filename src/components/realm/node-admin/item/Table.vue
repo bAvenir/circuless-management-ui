dbae543cc048376c1a415c7999032f3e9333de3f
Matej Ruzicka <matejruzicka@Matejs-MacBook-Pro.local>
2025-11-28 15:51:25 +0100

    filtering of TDs

--- src/components/realm/node-admin/item/Table.vue
+++ src/components/realm/node-admin/item/Table.vue
@@ -1,8 +1,37 @@
+<script lang="ts" setup>
+import { useTDFilter } from '~/composables/useTDFilter'
+import type { ThingDescription } from '~/api/realm/td'
+
+const { items, selectedItemId } = defineProps<{
+    items?: ThingDescription[]
+    selectedItemId?: string
+}>()
+
+const emit = defineEmits(['onSelect', 'onDelete'])
+
+const search = ref('')
+const selectedType = ref('all')
+
+const thingDescriptions = toRef(items ? { items } : { items: [] }, 'items')
+
+const { filtered } = useTDFilter(
+  thingDescriptions,
+  search,
+  selectedType
+)
+
+onMounted(() => {
+    if (selectedItemId) {
+        document.getElementById(selectedItemId)?.scrollIntoView({
+            behavior: 'smooth',
+        })
+    }
+})
+</script>
+
 <template>
     <div class="h-full w-full flex flex-col">
-        <div
-            class="pb-4 border-b border-slate-200 flex items-center gap-2 px-4 lg:px-0"
-        >
+        <div class="pb-4 border-b border-slate-200 flex items-center gap-2 px-4 lg:px-0">
             <IconField>
                 <InputIcon class="pi pi-search" />
                 <InputText
@@ -12,11 +41,12 @@
                 />
             </IconField>
         </div>
+
         <div class="lg:grow w-full lg:overflow-y-auto">
             <div
-                v-for="item in items"
+                v-for="item in filtered"
                 :key="item.id"
-                :id="`${item.id}`"
+                :id="item.id"
                 class="border-b border-slate-200 cursor-pointer hover:bg-realm-primary-100 transition-colors"
                 :class="{ 'bg-realm-primary-100': item.id === selectedItemId }"
                 @click="emit('onSelect', item.id)"
@@ -26,27 +56,3 @@
         </div>
     </div>
 </template>
-
-<script lang="ts" setup>
-import type { ThingDescription } from '~/api/realm/td'
-
-const { selectedItemId } = defineProps<{
-    items?: ThingDescription[]
-    selectedItemId?: string
-}>()
-
-const emit = defineEmits(['onSelect', 'onDelete'])
-
-onMounted(() => {
-    if (selectedItemId) {
-        const element = document.getElementById(selectedItemId)
-        if (element) {
-            element.scrollIntoView({ behavior: 'smooth' })
-        }
-    }
-})
-
-const search = ref('')
-</script>
-
-<style></style>
